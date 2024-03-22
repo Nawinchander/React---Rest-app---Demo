41ec678f325d4b7f06a663205a686eb75154838a
Nawin Chander S <56625450+Nawinchander@users.noreply.github.com>
2024-03-22 19:45:42 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,28 @@
+// import { useState } from 'react'
+import './App.css'
+import Home from './Components/Home'
+import About from './Components/About'
+import Work from './Components/Work'
+import Testimonial from './Components/Testimonial'
+import Footer from './Components/Footer'
+
+function App() {
+  
+  return (
+    <>
+      <div>
+        <Home />
+        <About />
+        <Work />
+        <Testimonial />
+        <Footer />
+
+       
+        
+      </div>
+      
+    </>
+  )
+}
+
+export default App
